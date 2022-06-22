c9494f17dd971ee56b4ea9e15a3529a5967097cc
David <davidolanrewaju538@gmail.com>
2022-06-22 08:13:14 +0100

    js and html files Added

---
+++ intro.js
@@ -0,0 +1,3 @@
+document.getElementById("name").innerHTML = "Hi, I am David Olanrewaju";
+document.getElementById("height").innerHTML = "I am 5'8 in height";
+document.getElementById("nationality").innerHTML = "And I'm Nigerian";
